3de819bde9d49b19ef7a18a47ab8016c453ab472
Dominykas Savickas <hexamarkovich@gmail.com>
2023-10-13 17:09:50 +0300

    V0.04 added function to export full Disk info, will need to join other hardware data retrieval functions

--- Multi_Platform_String_Search/Data/HardwareInfo.cs
+++ Multi_Platform_String_Search/Data/HardwareInfo.cs
@@ -28,4 +28,78 @@ public class CpuInfoService
         }
         return CPUusage;
     }
+    public List<string> RAMtype() //Returns RAM FormFactor and speed
+    {
+        var RAMtype = new List<string>();
+        var hardwareInfo = new HardwareInfo();
+        hardwareInfo.RefreshMemoryList();
+        foreach (var hardware in hardwareInfo.MemoryList)
+        {
+            RAMtype.Add(hardware.FormFactor.ToString());
+            RAMtype.Add(hardware.Speed.ToString());
+        }
+        return RAMtype;
+    }
+
+    public List<string> DISKtype() //Returns DISK usage in percentage
+    {
+        double SinglePartitionFreeSpace = 0;
+        double TotalPartitionFreeSpace = 0;
+        var FullInfo = new List<string>();
+
+        var hardwareInfo = new HardwareInfo();
+        hardwareInfo.RefreshDriveList();
+        foreach (var drive in hardwareInfo.DriveList)
+        {
+            string TempDname = drive.Model.ToString();
+            double DISKsize = Convert.ToDouble(drive.Size);
+            FullInfo.Add(TempDname);
+            FullInfo.Add(Math.Round(DISKsize / Math.Pow(10, 9), 2).ToString());
+            foreach (var partition in drive.PartitionList)
+            {
+                string TempPname = partition.Name.ToString();
+                foreach (var volume in partition.VolumeList)
+                {
+                    SinglePartitionFreeSpace += Convert.ToDouble(volume.FreeSpace);
+                    TotalPartitionFreeSpace += SinglePartitionFreeSpace;
+                }
+                FullInfo.Add(TempPname);
+                FullInfo.Add(Math.Round(TotalPartitionFreeSpace / Math.Pow(10, 9), 1).ToString());
+                SinglePartitionFreeSpace = 0;
+            }
+            FullInfo.Add(Math.Round(TotalPartitionFreeSpace / Math.Pow(10, 9), 1).ToString());
+            TotalPartitionFreeSpace = 0;
+        }
+
+        return FullInfo;
+    }
+
+    public string GPUusage() //Returns GPU usage in percentage
+    {
+        string GPUusage = "";
+        var hardwareInfo = new HardwareInfo();
+        hardwareInfo.RefreshCPUList();
+        foreach (var cpu in hardwareInfo.CpuList)
+        {
+            GPUusage = cpu.PercentProcessorTime.ToString();
+        }
+        return GPUusage;
+    }
+
+    public string RAMusage() //Returns RAM usage
+    {
+        string RAMusage;
+        double RAMtotal;
+        double RAMavailable;
+        double RAMmath;
+
+        var hardwareInfo = new HardwareInfo();
+        hardwareInfo.RefreshMemoryStatus();
+        RAMtotal = hardwareInfo.MemoryStatus.TotalPhysical;
+        RAMavailable = hardwareInfo.MemoryStatus.AvailablePhysical;
+        RAMmath = RAMavailable / RAMtotal * 100;
+        RAMusage = Math.Round(RAMmath).ToString();
+
+        return RAMusage;
+    }
 }
